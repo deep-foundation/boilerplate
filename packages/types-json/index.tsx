0df86b7006c10c4514045a3754be99f9f8fe65fe
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-07-18 13:35:16 +0300

    types-json reeadme

---
+++ packages/types-json/index.tsx
@@ -0,0 +1,15 @@
+export type Json =
+  | null
+  | boolean
+  | number
+  | string
+  | Json[]
+  | { [prop: string]: Json };
+
+export type JsonCompatible<T> = {
+  [P in keyof T]: T[P] extends Json
+    ? T[P]
+    : Pick<T, P> extends Required<Pick<T, P>>
+    ? never
+    : JsonCompatible<T[P]>;
+};
---
+++ packages/types-json/readme.md
@@ -0,0 +1,9 @@
+# deepcase types-json
+
+## usage
+
+```tsx
+import { Json, JsonCompatible } from '@deepcase/types-json';
+const a: Json;
+const b: JsonCompatible<string>
+```
