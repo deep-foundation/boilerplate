2ef7ea3452e7bf06dc38e449cfa7fead85797042
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-08-25 11:50:03 +0300

    0.0.14 auth doc

--- projects/app/imports/passport.ts
+++ projects/app/imports/passport.ts
@@ -1,23 +1,26 @@
 import { applyPassport } from '@deepcase/auth/passport/index';
 import github from '@deepcase/auth/passport/github';
 import LocalStrategy from 'passport-local';
+import Debug from 'debug';
+
+const debug = Debug('deepcase:passport');
 
 export const initPassport = () => applyPassport((passport) => {
   passport.use(github);
 
   passport.use(new LocalStrategy((username, password, done) => {
-    console.log('LocalStrategy', { username, password });
+    debug('LocalStrategy', { username, password });
     if (username === 'abc' && password === 'abc') done(null, { id: 'abc' });
     else done(null, false);
   }));
 
   passport.serializeUser((user, done) => {
-    console.log('serializeUser', user);
+    debug('serializeUser', user);
     done(null, { id: user.id });
   });
 
   passport.deserializeUser((id, done) => {
-    console.log('deserializeUser', id);
+    debug('deserializeUser', id);
     done(null, { id });
   });
 });
