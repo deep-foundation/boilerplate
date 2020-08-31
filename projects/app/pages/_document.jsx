06254ada9811e977a041007f49ddc51ede95f6ba
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-08-31 23:17:10 +0300

    fix passportSession

---
+++ projects/app/pages/_document.jsx
@@ -0,0 +1,75 @@
+// @flow
+
+import React from 'react';
+import _ from 'lodash';
+
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+
+import CleanCSS from 'clean-css';
+import autoprefixer from "autoprefixer";
+import postcss from 'postcss';
+
+import { defaultTheme } from '../imports/project/themes/default';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content="maximum-scale=1, minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+            key='viewport'
+          />
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={defaultTheme.palette.primary.main}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+if (!_.get(process, 'browser')) {
+  MyDocument.getInitialProps = async ctx => {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const css = sheets.toString();
+    const _prefixedCss = await postcss([
+      autoprefixer({ overrideBrowserslist: ['last 100 version'] }),
+    ]).process(css, { from: 'css.css', to: 'prefixedCss.css' });
+
+    let prefixedCss = _prefixedCss.css;
+    _prefixedCss.warnings().forEach(warn => {
+      prefixedCss = css;
+      console.warn(warn.toString());
+    });
+
+    return {
+      ...initialProps,
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          <style id="jss-server-size" dangerouslySetInnerHTML={{ __html: prefixedCss }}/>
+        </React.Fragment>,
+      ],
+    };
+  };
+}
--- projects/app/pages/dash.tsx
+++ projects/app/pages/dash.tsx
@@ -1,36 +1,37 @@
 import React, { useRef, useEffect } from 'react';
-// import Rete from 'rete';
-// import ReactRenderPlugin from 'rete-react-render-plugin';
-// import { createEditor } from '../imports/rete';
+import Rete from 'rete';
+import ReactRenderPlugin from 'rete-react-render-plugin';
+import { createEditor } from '../imports/rete';
 
-// class MyReactControl extends React.Component {
+class MyReactControl extends React.Component {
 
-//   componentDidMount() {
-//       // this.props.getData
-//       // this.props.putData
-//   }
+  componentDidMount() {
+      // this.props.getData
+      // this.props.putData
+  }
 
-//   render() {
-//     return (
-//         <div>Hello ${this.props.name}!</div>
-//     )
-//   }
-// }
+  render() {
+    return (
+        <div>Hello ${this.props.name}!</div>
+    )
+  }
+}
 
-// class MyControl extends Rete.Control {
-//   constructor(emitter, key, name) {
-//     super(key);
-//     this.render = 'react';
-//     this.component = MyReactControl;
-//     this.props = { emitter, name };
-//   }
-// }
+class MyControl extends Rete.Control {
+  constructor(emitter, key, name) {
+    super(key);
+    this.render = 'react';
+    this.component = MyReactControl;
+    this.props = { emitter, name };
+  }
+}
 
 export default function Page() {
   return <>
-    {/* <div
-      style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }}
+  123
+    <div
+      style={{ position: 'fixed', left: 0, top: 0, width: '100%', height: '100%' }}
       ref={(ref) => ref && createEditor(ref)}
-    /> */}
+    />
   </>;
 }
