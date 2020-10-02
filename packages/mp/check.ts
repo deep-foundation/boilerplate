918938f44f8e009667a303de77b0f453bae7d471
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-10-03 00:41:02 +0300

    main realization of materialized-path

---
+++ packages/mp/check.ts
@@ -0,0 +1,82 @@
+import { client } from './client';
+import Debug from 'debug';
+import { gql } from 'apollo-boost';
+import forEach from 'lodash/forEach';
+
+const debug = Debug('deepcase-mp:check');
+
+export const fetch = async () => {
+  const result = await client.query({ query: gql`query MyQuery {
+    mp: nodes__mp { id item_id path_item_depth path_item_id root_id position_id by_position(order_by: { path_item_depth: asc }) { id item_id path_item_depth path_item_id root_id position_id } }
+    nodes { from_id id to_id type in { from_id id to_id type } out { from_id id to_id type } }
+  }
+  ` });
+  return { nodes: result?.data?.nodes || [], mp: result?.data?.mp || [] };
+};
+
+interface Node {
+  from_id?: number; id?: number; to_id?: number; type?: number;
+  in: Node[]; out: Node[];
+}
+
+interface Marker {
+  id: number; item_id: number; path_item_depth: number; path_item_id: number; root_id: number; position_id: string;
+  by_position: Marker[];
+}
+
+export const check = async (hash: { [name:string]: number }) => {
+  const n: any = {};
+  forEach(hash, (value, key) => { n[value] = key });
+
+  const { nodes, mp } = await fetch();
+  debug('checking');
+  let valid = true;
+  const invalid = (...args) => {
+    valid = false;
+    debug(...args);
+  };
+  const nodesChecked: { [id: number]: boolean; } = {};
+  const markersChecked: { [id: number]: boolean; } = {};
+  const checkNode = (node: Node) => {
+    if (nodesChecked[node.id]) return;
+    else nodesChecked[node.id] = true;
+
+    const isLink = !!(node?.from_id && node?.to_id);
+    const isRoot = isLink ? false : !node?.in?.length;
+
+    const markers = mp.filter((m) => m.item_id === node.id);
+    const positions = mp.filter((m) => m.item_id === node.id && m.path_item_id === node.id);
+
+    debug(
+      `check #${n[node.id]} ${isLink ? 'link' : 'node'} in${node?.in?.length} out${node?.out?.length}`,
+      positions.map((pos) => {
+        return `${n[pos.root_id]} [${pos.by_position.map((m) => `${n[m.path_item_id]}`).join(',')}]`;
+      }),
+    );
+
+    if (isRoot) {
+      if (markers.length !== 1) invalid(`invalid node #${n[node.id]} root but markers.length = ${markers.length}`);
+    }
+
+    if (!markers.length) invalid(`invalid node #${n[node.id]} markers lost, markers.length = ${markers.length}`);
+
+    positions.forEach((position) => {
+      checkPosition(position);
+    });
+  };
+  const checkPosition = (position: Marker) => {
+    position.by_position.forEach((marker, i) => {
+      markersChecked[marker.id] = true;
+      if (marker.position_id != position.position_id) invalid(`invalid position ${n[position.root_id]} [${position.by_position.map((m) => n[m.path_item_id]).join(',')}] position_id not equal`);
+      const node = nodes.find((n) => n.id === marker.path_item_id);
+      if (!node) invalid(`invalid position ${n[position.root_id]} [${position.by_position.map((m) => n[m.path_item_id]).join(',')}] node lost #${n[marker.path_item_id]}`);
+    });
+  };
+  nodes.forEach((node) => {
+    checkNode(node);
+  });
+  mp.forEach((marker) => {
+    if (!markersChecked[marker.id]) invalid(`invalid marker #${marker.id} of node #${n[marker.item_id]} ${n[marker.root_id]}[...${n[marker.path_item_id]}...]`);
+  });
+  if (!valid) throw new Error('invalid');
+};
---
+++ packages/mp/test.ts
@@ -0,0 +1,153 @@
+import { client } from './client';
+import Debug from 'debug';
+import { gql } from 'apollo-boost';
+import { check } from './check';
+
+const debug = Debug('deepcase-mp:test');
+
+const insertNode = async () => {
+  const result = await client.mutate({ mutation: gql`mutation InsertNode {
+    insert_nodes(objects: {}) { returning { id } }
+  }` });
+  const id = result?.data?.insert_nodes?.returning?.[0]?.id;
+  debug(`insert node #${id}`);
+  return id;
+};
+const insertLink = async (fromId: number, toId: number) => {
+  const result = await client.mutate({ mutation: gql`mutation InsertLink($fromId: Int, $toId: Int) {
+    insert_nodes(objects: { from_id: $fromId, to_id: $toId }) { returning { id } }
+  }`, variables: { fromId, toId } });
+  const id = result?.data?.insert_nodes?.returning?.[0]?.id;
+  debug(`insert link #${id} (#${fromId} -> #${toId})`);
+  return id;
+};
+const clear = async () => {
+  await client.mutate({ mutation: gql`mutation Clear {
+    delete_nodes(where: {}) { affected_rows }
+    delete_nodes__mp(where: {}) { affected_rows }
+  }
+  ` });
+  debug(`clear`);
+};
+const deleteNode = async (id: number) => {
+  const result = await client.mutate({ mutation: gql`mutation DeleteNode($id: Int) {
+    delete_nodes(where: { id: { _eq: $id } }) { returning { id } }
+  }`, variables: { id } });
+  debug(`delete node #${id}`);
+  return result?.data?.delete_nodes?.returning?.[0]?.id;
+};
+
+it('+1', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  await check({ a, b });
+});
+it('-1', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  await deleteNode(a);
+  await check({ a, b });
+});
+it('+2', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  await check({ a, b, c });
+});
+it('-2', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  await deleteNode(c);
+  await check({ a, b, c });
+});
+it('+3', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  await check({ a, b, c, d, e });
+});
+it('-3', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  await deleteNode(e);
+  await check({ a, b, c, d, e });
+});
+it('+4', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const x = await insertNode();
+  const y = await insertLink(x, a);
+  await check({ a, b, c, d, e, x, y });
+});
+it('-4', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const x = await insertNode();
+  const y = await insertLink(x, a);
+  await deleteNode(y);
+  await check({ a, b, c, d, e, x, y });
+});
+it('+5', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const x = await insertNode();
+  const y = await insertLink(x, b);
+  await check({ a, b, c, d, e, x, y });
+});
+it('-5', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const x = await insertNode();
+  const y = await insertLink(x, b);
+  await deleteNode(y);
+  await check({ a, b, c, d, e, x, y });
+});
+it('+7', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const y = await insertLink(a, d);
+  await check({ a, b, c, d, e, y });
+});
+it('-7', async () => {
+  await clear();
+  const a = await insertNode();
+  const b = await insertNode();
+  const c = await insertLink(a, b);
+  const d = await insertNode();
+  const e = await insertLink(b, d);
+  const y = await insertLink(a, d);
+  await deleteNode(y);
+  await check({ a, b, c, d, e, y });
+});
