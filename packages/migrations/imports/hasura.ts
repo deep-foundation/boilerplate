4418f22c142d6d917cab33733c7ff157f9a5fb87
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-11-14 16:22:12 +0000

    https://github.com/deepcase/boilerplate/issues/30

--- packages/migrations/imports/hasura.ts
+++ packages/migrations/imports/hasura.ts
@@ -9,17 +9,10 @@ const HASURA_SECRET = process.env.HASURA_SECRET;
 if (!HASURA_PATH) throw new Error('!HASURA_PATH');
 
 const hasura = {
-  sql: (sql: string) => axios({
-    method: 'post',
-    url: `http${HASURA_SSL ? 's' : ''}://${HASURA_PATH}/v1/query`,
-    headers: {
-      ...(HASURA_SECRET ? { 'x-hasura-admin-secret': HASURA_SECRET } : {}),
-    },
-    data: {
-      type: 'run_sql',
-      args: {
-        sql,
-      },
+  sql: (sql: string) => hasura.query({
+    type: 'run_sql',
+    args: {
+      sql,
     },
   }),
   query: (data: any) => axios({
