97b0798f01fd63a59fbebccbf0731679ee2800d2
Ivan S Glazunov <ivansglazunov@gmail.com>
2020-11-17 14:34:30 +0300

    currect error handling

--- packages/migrations/imports/hasura.ts
+++ packages/migrations/imports/hasura.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import Debug from 'debug';
 
-axios.defaults.validateStatus = () => true;
+const debug = Debug('deepcase:migrations');
 
 const HASURA_PATH = process.env.HASURA_PATH;
 const HASURA_SSL = !!+process.env.HASURA_SSL;
@@ -8,21 +9,38 @@ const HASURA_SECRET = process.env.HASURA_SECRET;
 
 if (!HASURA_PATH) throw new Error('!HASURA_PATH');
 
-const hasura = {
+export interface HasuraAxiosResponse extends AxiosResponse {
+  error?: string;
+}
+
+export const hasura = {
+  validateStatus: () => true,
+  getError: (result: AxiosResponse): null | string => {
+    const { status } = result;
+    const error = status >= 200 && status < 300 ? null : result.statusText;
+    return error;
+  },
   sql: (sql: string) => hasura.query({
     type: 'run_sql',
     args: {
       sql,
     },
   }),
-  query: (data: any) => axios({
-    method: 'post',
-    url: `http${HASURA_SSL ? 's' : ''}://${HASURA_PATH}/v1/query`,
-    headers: {
-      ...(HASURA_SECRET ? { 'x-hasura-admin-secret': HASURA_SECRET } : {}),
-    },
-    data,
-  }),
+  query: async (data: any) => {
+    debug('hasura:query', data?.type);
+    const result: HasuraAxiosResponse = await axios({
+      method: 'post',
+      url: `http${HASURA_SSL ? 's' : ''}://${HASURA_PATH}/v1/query`,
+      headers: {
+        ...(HASURA_SECRET ? { 'x-hasura-admin-secret': HASURA_SECRET } : {}),
+      },
+      data,
+      validateStatus: hasura.validateStatus,
+    });
+    result.error = hasura.getError(result);
+    if (result.error) debug('error', result);
+    return result;
+  },
 };
 
 export const sql = (strings: TemplateStringsArray, ...expr: any[]) =>
